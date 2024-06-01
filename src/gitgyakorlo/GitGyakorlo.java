1ea7de7793dae18de7e9a3a59f9fdbb900532b24
Konrad <molnar.konrad@gmail.com>
2024-06-01 09:05:48 +0200

    void feladatok() létrehozva

--- src/gitgyakorlo/GitGyakorlo.java
+++ src/gitgyakorlo/GitGyakorlo.java
@@ -3,7 +3,11 @@ package gitgyakorlo;
 public class GitGyakorlo {
 
     public static void main(String[] args) {
+        feladatok();
+    }
 
+    private static void feladatok() {
+        
     }
     
 }
